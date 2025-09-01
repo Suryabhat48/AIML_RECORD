66236b7868ae49043650fa534814a3447c09eb1f
Suryabhat48 <suryabhat2032@gmail.com>
2025-09-01 17:33:42 +0530

    Make some of the changes in the program

--- python.py
+++ python.py
@@ -1,1 +1,8 @@
 print("Hello, World!\n")
+# DIY :
+# 2.1 Create a variable and assign it a string value
+message="hello surya"
+print(message)
+# 2.2 Print the variable
+new_message="Surya is a great guy"
+print(new_message)
